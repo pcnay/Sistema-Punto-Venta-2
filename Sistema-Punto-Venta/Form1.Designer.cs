1c00d584083187d66e88c48ff699ac4ac3faf6cb
Ramon <Ramon_Ortega2233@jabil.com>
2020-08-12 17:28:22 -0700

    se comienza a modificar la ventana principal, se agregan Tabcontrol de Ventas y Clientes, se inician los primeros ajustes.

--- Sistema-Punto-Venta/Form1.Designer.cs
+++ Sistema-Punto-Venta/Form1.Designer.cs
@@ -30,8 +30,10 @@
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.ventas = new System.Windows.Forms.TabPage();
+            this.Cliente = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.buttonCliente = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.tabControl1.SuspendLayout();
             this.SuspendLayout();
@@ -50,33 +52,53 @@
             // tabControl1
             // 
             this.tabControl1.Alignment = System.Windows.Forms.TabAlignment.Bottom;
-            this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.ventas);
+            this.tabControl1.Controls.Add(this.Cliente);
             this.tabControl1.Location = new System.Drawing.Point(0, 113);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
             this.tabControl1.Size = new System.Drawing.Size(1900, 900);
             this.tabControl1.TabIndex = 1;
             // 
-            // tabPage1
+            // ventas
             // 
-            this.tabPage1.Location = new System.Drawing.Point(4, 4);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(1892, 874);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
-            this.tabPage1.UseVisualStyleBackColor = true;
+            this.ventas.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ventas.Location = new System.Drawing.Point(4, 4);
+            this.ventas.Name = "ventas";
+            this.ventas.Padding = new System.Windows.Forms.Padding(3);
+            this.ventas.Size = new System.Drawing.Size(1892, 874);
+            this.ventas.TabIndex = 0;
+            this.ventas.Text = "Ventas";
+            this.ventas.UseVisualStyleBackColor = true;
             // 
-            // tabPage2
+            // Cliente
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 4);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(975, 874);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
-            this.tabPage2.UseVisualStyleBackColor = true;
+            this.Cliente.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Cliente.Location = new System.Drawing.Point(4, 4);
+            this.Cliente.Name = "Cliente";
+            this.Cliente.Padding = new System.Windows.Forms.Padding(3);
+            this.Cliente.Size = new System.Drawing.Size(1892, 874);
+            this.Cliente.TabIndex = 1;
+            this.Cliente.Text = "Clientes";
+            this.Cliente.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // buttonCliente
+            // 
+            this.buttonCliente.Location = new System.Drawing.Point(12, 56);
+            this.buttonCliente.Name = "buttonCliente";
+            this.buttonCliente.Size = new System.Drawing.Size(107, 35);
+            this.buttonCliente.TabIndex = 3;
+            this.buttonCliente.Text = "Cliente";
+            this.buttonCliente.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -84,10 +106,13 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.ClientSize = new System.Drawing.Size(984, 961);
+            this.Controls.Add(this.buttonCliente);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Cliente";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.tabControl1.ResumeLayout(false);
@@ -99,8 +124,10 @@
 
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.TabControl tabControl1;
-        private System.Windows.Forms.TabPage tabPage1;
-        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage ventas;
+        private System.Windows.Forms.TabPage Cliente;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonCliente;
     }
 }
 
